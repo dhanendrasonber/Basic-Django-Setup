46e79c9976744e7803b0ff91842d18da331dac0b
Dhanendra Kumar Sonber <dhanendrakumarsonber123@gmail.com>
2024-04-17 09:43:59 +0530

    File for djangorestframework

---
+++ my_script.ps1
@@ -0,0 +1,22 @@
+param (
+    [string]$projectName
+)
+
+if (-not $projectName) {
+    $projectName = Read-Host "Enter project name"
+}
+
+# Create virtual environment (Python 3)
+python -m venv venv
+
+# Activate virtual environment
+.\venv\Scripts\Activate
+
+# Install Django
+pip install django
+
+# Install Django REST framework
+pip install djangorestframework
+
+# Start Django project
+django-admin startproject $projectName
